7c4d6d19f9bf168a77e20af1bb6559c2575a456b
Franklin Kong <franklinkong981@gmail.com>
2024-09-01 15:48:16 -0700

    Wrote /items POST

--- routes/shoppingList.js
+++ routes/shoppingList.js
@@ -9,6 +9,16 @@ router.get("/", function(req, res) {
   res.json({items});
 });
 
-
+router.post("/", function(req, res, next) {
+  try {
+    if (!req.body.name) throw new ExpressError("Name is required", 400);
+    if (!req.body.price) throw new ExpressError("Price is required", 400);
+    const newItem = {name: req.body.name, price: req.body.price};
+    items.push(newItem);
+    return res.status(201).json({added: newItem});
+  } catch(e) {
+    return next(e);
+  }
+});
 
 module.exports = router;
